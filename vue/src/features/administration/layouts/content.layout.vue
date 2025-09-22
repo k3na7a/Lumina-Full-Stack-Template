6e2821667dbc4e5a0a841a51a71ed683df126a63
John Desjardins <desjjoh@gmail.com>
2025-09-22 18:49:49 -0400

    Breadcrumbs moved to top of header component

--- vue/src/features/administration/layouts/content.layout.vue
+++ vue/src/features/administration/layouts/content.layout.vue
@@ -1,13 +1,4 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-import { useRoute } from 'vue-router'
-
-const route = useRoute()
-
-const breadcrumbs = computed(() => {
-  return route.meta.breadcrumbs ?? []
-})
-
 type proptype = {
   title: string
   subtitle: string
@@ -23,33 +14,7 @@ const { title, subtitle } = defineProps<proptype>()
       <p class="text-muted fw-normal">{{ $t(subtitle) }}</p>
     </div>
     <div class="card d-flex flex-column p-3">
-      <div class="d-flex flex-column gap-3">
-        <div v-if="breadcrumbs.length" class="bg-alt3 px-2 py-1">
-          <ol
-            class="d-flex flex-row gap-2 flex-wrap m-0 p-0 align-items-center text-primary"
-            style="list-style-type: none"
-          >
-            <template v-for="(crumb, idx) in breadcrumbs" :key="idx">
-              <li>
-                <RouterLink
-                  v-if="crumb.to"
-                  :to="{ name: crumb.to }"
-                  class="link-offset-2 link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
-                >
-                  <p>{{ $t(crumb.name) }}</p>
-                </RouterLink>
-                <p v-else class="text-light-alt">{{ $t(crumb.name) }}</p>
-              </li>
-              <template v-if="idx < breadcrumbs.length - 1">
-                <li class="text-muted">
-                  <font-awesome-icon size="sm" :icon="['fas', 'chevron-right']" />
-                </li>
-              </template>
-            </template>
-          </ol>
-        </div>
-        <slot name="table"></slot>
-      </div>
+      <slot name="table"></slot>
     </div>
   </div>
 </template>
--- vue/src/shared/components/header/topnav-header.component.vue
+++ vue/src/shared/components/header/topnav-header.component.vue
@@ -3,11 +3,44 @@ import { sub_navigation } from '@/shared/components/navigation/types/sub-navigat
 
 type props = { title: string; subtitle?: string; routes: sub_navigation }
 
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const breadcrumbs = computed(() => {
+  return route.meta.breadcrumbs ?? []
+})
+
 const { title, subtitle, routes } = defineProps<props>()
 </script>
 
 <template>
   <header class="th-header d-flex flex-column gap-1 p-3 pb-0">
+    <div v-if="breadcrumbs.length">
+      <ol class="d-flex flex-row gap-2 flex-wrap m-0 p-0 align-items-center text-primary" style="list-style-type: none">
+        <li class="text-muted">
+          <font-awesome-icon size="lg" :icon="['far', 'bookmark']" />
+        </li>
+        <template v-for="(crumb, idx) in breadcrumbs" :key="idx">
+          <li>
+            <RouterLink
+              v-if="crumb.to"
+              :to="{ name: crumb.to }"
+              class="link-offset-2 link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+            >
+              <p>{{ $t(crumb.name) }}</p>
+            </RouterLink>
+            <p v-else class="text-light-alt">{{ $t(crumb.name) }}</p>
+          </li>
+          <template v-if="idx < breadcrumbs.length - 1">
+            <li class="text-muted">
+              <font-awesome-icon size="sm" :icon="['fas', 'chevron-right']" />
+            </li>
+          </template>
+        </template>
+      </ol>
+    </div>
     <div class="d-flex flex-column">
       <h2 class="fw-bold display-font text-truncate">
         {{ $t(title) }}
